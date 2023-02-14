d4ddbe3e83dc10a5eb6ee0c500d357d89f22e03e
kelo221 <villekum@metropolia.fi>
2023-02-14 19:23:28 +0200

    ~10x faster execution, clean up

--- go.mod
+++ go.mod
@@ -1,8 +1,5 @@
 module commandLine
 
-go 1.18
+go 1.20
 
-require (
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
-	golang.org/x/image v0.0.0-20220617043117-41969df76e82
-)
+require golang.org/x/image v0.4.0
--- main.go
+++ main.go
@@ -1,76 +1,135 @@
 package main
 
 import (
-	"fmt"
 	"golang.org/x/image/draw"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/basicfont"
 	"golang.org/x/image/math/fixed"
 	"image"
 	"image/color"
-	"image/jpeg"
 	"image/png"
+	"log"
 	"math"
 	"math/rand"
 	"os"
-	"time"
 )
 
-const imageHeight = 20
-const imageWidth = 48
-const capchaLenght = 6
-const scaleFactor = 20
-const noiseLevel = 0
-const pngOutput = true
+const characterWidth = 7
+const characterHeight = 13
+const characterCount = 8
 
-type Pixel struct {
-	R int
-	G int
-	B int
-	A int
+const imageSizeMultiplier = 4
+
+const imageWidth = characterCount*characterWidth + 1
+const imageHeight = characterHeight + 44
+
+func main() {
+
+	drawer, img := createImage()
+	drawer.DrawString(generateRandomString(characterCount))
+
+	img = resizeImage(img)
+	img = distortImage(img)
+	img = addNoise(img)
+
+	saveImage(*img)
+
 }
 
-func addLabel(img *image.RGBA, x, y int, label string) {
-	col := color.RGBA{
-		R: uint8(50 + rand.Intn(120-50)),
-		G: uint8(50 + rand.Intn(120-50)),
-		B: uint8(50 + rand.Intn(120-50)),
-		A: 255,
-	}
-	point := fixed.Point26_6{X: fixed.I(x), Y: fixed.I(y)}
+func addNoise(img *image.Gray) *image.Gray {
+	imageArray := imageToArray(img)
 
-	d := &font.Drawer{
+	for y, h := range imageArray {
+		for x := range h {
+			img.Set(x, y, color.Gray{Y: imageArray[y][x] + uint8(rand.Intn(255-0)+0)/2})
+		}
+	}
+	return img
+}
+
+func distortImage(src *image.Gray) *image.Gray {
+
+	sineModifier := 0.1 + rand.Float64()*(.9-0.1)
+	var sineCollection []float64
+
+	// generate height modifier for each column
+	for i := 0.1; i < imageHeight*imageSizeMultiplier*20; i += .05 {
+		sineCollection = append(sineCollection, math.Sin(i*sineModifier))
+	}
+
+	imageArray := imageToArray(src)
+	modifierRange := 20.0
+
+	for y, h := range imageArray {
+		for x := range h {
+			if imageArray[y][x] != 255 {
+				src.Set(x, y, color.Gray{Y: 255})
+				arrayMod := int(math.Round(modifierRange*sineCollection[x])) + int(modifierRange)
+				src.Set(x, (((arrayMod+imageHeight*imageSizeMultiplier)+y)/2)-(rand.Intn((2*imageSizeMultiplier/2)-0)+0)-imageHeight/2, color.Gray{Y: 100})
+			}
+
+		}
+	}
+
+	return src
+}
+
+func resizeImage(src image.Image) *image.Gray {
+	dst := image.NewGray(image.Rect(0, 0, imageWidth*imageSizeMultiplier, imageHeight*imageSizeMultiplier))
+	draw.NearestNeighbor.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
+	return dst
+}
+
+func createImage() (*font.Drawer, *image.Gray) {
+
+	img := image.NewGray(image.Rect(0, 0, imageWidth, imageHeight))
+	draw.Draw(img, img.Bounds(), image.White, image.Point{}, draw.Src)
+
+	face := basicfont.Face7x13
+	drawer := &font.Drawer{
 		Dst:  img,
-		Src:  image.NewUniform(col),
-		Face: basicfont.Face7x13,
-		Dot:  point,
+		Src:  image.Black,
+		Face: face,
+		Dot:  fixed.Point26_6{X: characterWidth * 9, Y: characterHeight * 160},
 	}
-	d.DrawString(label)
+
+	return drawer, img
 }
 
-func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
-	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+func generateRandomString(captchaLength int) string {
+
+	characterSet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var charArray = make([]uint8, captchaLength)
+
+	for i := 0; i < captchaLength; i++ {
+		charArray[i] = characterSet[rand.Intn(len(characterSet))]
+	}
+	captchaCharacters := string(charArray[:])
+
+	return captchaCharacters
 }
 
-func pixelToRGBA(pixel Pixel) color.RGBA {
-	return color.RGBA{
-		R: uint8(pixel.R * 257),
-		G: uint8(pixel.G * 257),
-		B: uint8(pixel.B * 257),
-		A: uint8(pixel.A * 257),
+func saveImage(img image.Gray) {
+	f, err := os.Create("hello.png")
+	if err != nil {
+		log.Fatalf("Failed to create file: %v", err)
+	}
+	err = png.Encode(f, &img)
+	if err != nil {
+		log.Fatalf("Failed to encode image: %v", err)
 	}
 }
 
-func imageToArray(img *image.RGBA) [][]Pixel {
+func imageToArray(img *image.Gray) [][]uint8 {
 
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
 
-	var pixels [][]Pixel
+	var pixels [][]uint8
 	for y := 0; y < height; y++ {
-		var row []Pixel
+		var row []uint8
 		for x := 0; x < width; x++ {
-			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+			row = append(row, grayToUint8(img.At(x, y).RGBA()))
 		}
 		pixels = append(pixels, row)
 	}
@@ -78,108 +137,6 @@ func imageToArray(img *image.RGBA) [][]Pixel {
 	return pixels
 }
 
-func main() {
-
-	rand.Seed(time.Now().UnixNano())
-
-	charset := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	var c [capchaLenght]uint8
-
-	for i := 0; i < capchaLenght; i++ {
-		c[i] = charset[rand.Intn(len(charset))]
-	}
-
-	capchaCharacters := string(c[:])
-
-	fmt.Println(capchaCharacters)
-
-	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
-	addLabel(img, capchaLenght-2, imageHeight/2+10, capchaCharacters)
-
-	var fileExtension string
-
-	if pngOutput {
-		fileExtension = "png"
-	} else {
-		fileExtension = "jpg"
-	}
-
-	filename := "capcha." + fileExtension
-
-	output, _ := os.Create(filename)
-	defer output.Close()
-
-	dst := image.NewRGBA(image.Rect(0, 0, imageWidth*scaleFactor, imageHeight*scaleFactor))
-	draw.NearestNeighbor.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
-
-	v := 0.1 + rand.Float64()*(.9-0.1)
-	fmt.Println("sine variable", v)
-
-	var sineCollection []float64
-
-	// generate sine values into an array
-	for i := 0.1; i < 480; i += .05 {
-		sineCollection = append(sineCollection, math.Sin(i*v))
-	}
-
-	array := imageToArray(dst)
-
-	// 24 total pixel to move
-	// 1 to -1, range of 2
-	// each unit = 8 pixels
-	// if the pixel is not empty get the sine value for the current column
-	// move the current pixels up or down depending on sine value
-	// clear the old pixels
-	// +8 to offset to 0
-
-	modifierRange := math.Round(240) / 10
-
-	// You cannot modify constants
-	arrayCountHorizontal := imageWidth
-	arrayCountVertical := imageHeight
-
-	fmt.Println(arrayCountHorizontal * scaleFactor)
-
-	for x := 0; x < (arrayCountHorizontal * scaleFactor); x++ {
-		for y := 0; y < (arrayCountVertical * scaleFactor); y++ {
-			if array[y][x] != (Pixel{0, 0, 0, 0}) {
-				sineModifier := int(math.Round(modifierRange*sineCollection[x])) + int(modifierRange)
-				array[((sineModifier+arrayCountVertical*4)+y)/2][x] = array[y][x]
-				array[y][x] = Pixel{0, 0, 0, 0}
-			}
-		}
-	}
-
-	if noiseLevel > 0 {
-		for x := 0; x < (arrayCountHorizontal * scaleFactor); x++ {
-			for y := 0; y < (arrayCountVertical * scaleFactor); y++ {
-				array[y][x].R = (array[y][x].R + noiseLevel*(0+rand.Intn(255-0))) / (1 + noiseLevel)
-				array[y][x].G = (array[y][x].G + noiseLevel*(0+rand.Intn(255-0))) / (1 + noiseLevel)
-				array[y][x].B = (array[y][x].B + noiseLevel*(0+rand.Intn(255-0))) / (1 + noiseLevel)
-				array[y][x].A = 255
-			}
-		}
-	}
-
-	newImage := image.NewRGBA(image.Rect(0, 0, img.Bounds().Max.X*scaleFactor, img.Bounds().Max.Y*scaleFactor))
-
-	// convert back to RGBA
-	for x := 0; x < imageHeight*scaleFactor; x++ {
-		for y := 0; y < imageWidth*scaleFactor; y++ {
-			newImage.SetRGBA(y, x, pixelToRGBA(array[x][y]))
-		}
-	}
-
-	// Set the expected size that you want:
-	dst2 := image.NewRGBA(image.Rect(0, 0, newImage.Bounds().Max.X/3, newImage.Bounds().Max.Y/3))
-
-	// Resize:
-	draw.NearestNeighbor.Scale(dst2, dst2.Rect, newImage, newImage.Bounds(), draw.Over, nil)
-
-	if !pngOutput {
-		jpeg.Encode(output, dst2, &jpeg.Options{Quality: 90})
-	} else {
-		png.Encode(output, dst2)
-	}
-
+func grayToUint8(r uint32, g uint32, b uint32, a uint32) uint8 {
+	return uint8((r + g + b) / 3)
 }
